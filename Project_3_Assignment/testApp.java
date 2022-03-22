1a948fc161635e277fb9c87b810df14853cb313c
thom1664 <55003411+thom1664@users.noreply.github.com>
2022-03-22 11:09:56 -0600

    testApp Class

--- Project_3_Assignment/testApp.java
+++ Project_3_Assignment/testApp.java
@@ -2,21 +2,27 @@ import java.util.Scanner;
 
 public class testApp{
     public static void main(String args[]){
-        int seedVal, processNum, lastAT, maxBT, QuantSize, latency = 0;
+        String change1, change2, change3, change4, change5, change6;
         Scanner reader = new Scanner(System.in);
 
         System.out.println("Enter a seed value:");
-        seedVal = reader.nextLine();
+        change1 = reader.nextLine();
+        Integer seedVal = Integer.valueOf(change1);
         System.out.println("Enter number of processes (2, 100):");
-        processNum = reader.nextLine();
+        change2 = reader.nextLine();
+        Integer processNum = Integer.valueOf(change2);
         System.out.println("Enter last possible arrival time (0, 99):");
-        lastAT = reader.nextLine();
+        change3 = reader.nextLine();
+        Integer lastAT = Integer.valueOf(change3);
         System.out.println("Enter max burst time (1, 100):");
-        maxBT = reader.nextLine();
+        change4 = reader.nextLine();
+        Integer maxBT = Integer.valueOf(change4);
         System.out.println("Enter quantum size (1, 100):");
-        QuantSize = reader.nextLine();
+        change5 = reader.nextLine();
+        Integer QuantSize = Integer.valueOf(change5);
         System.out.println("Enter latency (0, 10):");
-        latency = reader.nextLine();
+        change6 = reader.nextLine();
+        Integer latency = Integer.valueOf(change6);
 
         System.out.println("Your inputs included:");
         System.out.print(System.lineSeparator());
@@ -24,8 +30,13 @@ public class testApp{
         System.out.print(System.lineSeparator());
         System.out.println(processNum +"processes");
         System.out.print(System.lineSeparator());
-        System.out.print("A last possible arrival time of"+ lastAT);
-
+        System.out.println("A last possible arrival time of"+ lastAT);
+        System.out.print(System.lineSeparator());
+        System.out.println("A max burst time of"+ maxBT);
+        System.out.print(System.lineSeparator());
+        System.out.println("A quantum size of"+ QuantSize);
+        System.out.print(System.lineSeparator());
+        System.out.println("A latency of"+ latency);
     }
 
 }
