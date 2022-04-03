4a8756903e560ecd246c5781a36195ac95162c5e
thom1664 <55003411+thom1664@users.noreply.github.com>
2022-04-03 15:50:14 -0600

    Project_4

---
+++ Project_4_Assignment/Project_4/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+
+    public class OnePerson(int id, int gender, int time) {
+        Arrive(id, gender);
+        UseFacilities(id, gender, time);
+        Depart(id, gender);
+        } 
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
