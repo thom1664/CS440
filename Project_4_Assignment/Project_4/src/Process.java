2069cbba58a690a9b31c3227520519c0d06d842d
thom1664 <55003411+thom1664@users.noreply.github.com>
2022-04-13 01:05:27 -0600

    Arrive and Depart functionality

--- Project_4_Assignment/Project_4/src/Process.java
+++ Project_4_Assignment/Project_4/src/Process.java
@@ -1,16 +1,46 @@
 import java.util.LinkedList;
+import java.util.Random;
 
 public class Process extends OnePerson{
-    public void Arrive(id, gender) throws InterruptedException {
-        
+    private LinkedList<Integer> list = new LinkedList();
+    private final int LIMIT = 10;
+    private Object lock = new Object();
+
+    public Process(int id, int gender, int time) {
+        super(id, gender, time);
+        //TODO Auto-generated constructor stub
     }
 
-    public UseFacilities(id, gender, time){
+    public void Arrive(int id, int gender) throws InterruptedException {
+        while(true){
+            synchronized(lock){
+                while(list.size() == LIMIT){
+                    lock.wait();
+                }
+            }
+            list.add(id++);
+            lock.notify();
+        }
+    }
+
+    public UseFacilities(int id, int gender, int time){
 
     }
 
-    public void Depart(id, gender) throws InterruptedException {
+    public void Depart(int id, int gender) throws InterruptedException {
+        Random random = new Random();
 
+        while(true){    
+            synchronized(lock){
+                while(list.size() == 0){
+                    lock.wait();
+                }
+                System.out.print("List size is: " + list.size());
+                id = list.removeFirst();
+                System.out.println("; ");
+                lock.notify();
+            }
+        }
     }
 
     public static void main(String[] args) throws Exception {
