6d34dedbc3f763e3843004678ae90b1e57411974
thom1664 <55003411+thom1664@users.noreply.github.com>
2022-04-13 00:00:03 -0600

    Fixed structure

---
+++ Project_4_Assignment/Project_4/src/OnePerson.java
@@ -0,0 +1,17 @@
+public class OnePerson(int id, int gender, int time){
+    public Arrive(id, gender){
+
+    }
+
+    public UseFacilities(id, gender, time){
+
+    }
+
+    public Depart(id, gender){
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
